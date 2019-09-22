1ed168f356a1a44a8fb068ad44b9adc2b93dff82
kan_a_17 <kan.artem.web.dev@gmail.com>
2019-09-22 11:56:55 +0500

    v&2k merge

---
+++ scripts/gallery.js
@@ -0,0 +1,69 @@
+//@ts-check
+(function runGalleryJs() {
+  function createGallery(props) {
+    const { id, imagesData, popupId } = props;
+
+    const gallery = document.getElementById(id);
+    const popup = document.getElementById(popupId);
+    const images = loadImages(imagesData);
+    const cards = buildCards(images, popup);
+
+    popup.addEventListener('click', (e) => {
+      if (e.target === popup) {
+        popup.classList.remove('show');
+      }
+    });
+
+    cards.forEach((card) => gallery.appendChild(card));
+  }
+
+  function buildCards(images, popup) {
+    return images.map((image, i) => {
+      const photocard = document.createElement('div');
+
+      photocard.classList.add('photo');
+      image.classList.add('photo-img');
+
+      photocard.appendChild(image);
+
+      photocard.addEventListener('click', () => {
+        popupImage(popup, image);
+      });
+
+      return photocard;
+    });
+  }
+
+  function loadImages(imagesData) {
+    return imagesData.map((data) => {
+      const { desc, url, fullUrl } = data;
+      const img = new Image();
+      img.src = url;
+      img.setAttribute('data-url-full', fullUrl);
+      return img;
+    });
+  }
+
+  function popupImage(el, image) {
+    const img = new Image();
+    img.src = image.getAttribute('data-url-full');
+    el.children[0].innerHTML = '';
+    el.children[0].appendChild(img);
+    el.classList.add('show');
+  }
+
+  function detectScrollWidth() {
+    const div = document.createElement('div');
+    div.style.opacity = '0';
+    div.style.position = 'fixed';
+    div.style.zIndex = '-999';
+    div.style.overflow = 'scroll';
+    div.style.width = '100px';
+
+    document.body.appendChild(div);
+    return 100 - div.clientWidth;
+  }
+
+  //@ts-ignore
+  window.createGallery = createGallery;
+})();

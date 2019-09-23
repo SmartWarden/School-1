9dac2a4f2ae4804ac8c0a9d380655941dcad0f15
kan_a_17 <kan.artem.web.dev@gmail.com>
2019-09-23 07:40:19 +0500

    23/09 07:40

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,11 +1,54 @@
 //@ts-check
 (function runGalleryJs() {
+  class ImageLoader {
+    constructor(buffersize, maxAttempts) {
+      this.queue = [];
+      this.buffersize = buffersize;
+      this.maxAttempts = maxAttempts;
+    }
+
+    addToQueue(url, importance=0, img, attempts=0) {
+      if (img == null) img = new Image;
+      const queueEl = { img, url, importance };
+      const { queue, maxAttempts } = this;
+
+      const remove = this._removeFromQueue.bind(null, img);
+      img.addEventListener('load', remove);
+      img.addEventListener('error', () => {
+        if (attempts > maxAttempts) {
+          console.error(`Image ${url} couldn't be loaded.`);
+          remove();
+        } else {
+          this.addToQueue(url, importance, img, attempts + 1);
+        }
+      });
+
+      for (let i = 0, len = queue.length; i < len; i++) {
+        const current = queue[i];
+        if (current.importance < importance || i === len - 1) {
+          queue.splice(i, 0, queueEl);
+          break;
+        }
+      }
+    }
+
+    _removeFromQueue(img) {
+      const { queue } = this;
+      const i = queue.findIndex(img);
+      queue.splice(i, 1);
+    }
+
+    _load() {
+      
+    }
+  }
+
   function createGallery(props) {
     const { id, imagesData, popupId } = props;
 
     const gallery = document.getElementById(id);
     const popup = document.getElementById(popupId);
-    const images = loadImages(imagesData);
+    const images = buildImages(imagesData);
     const cards = buildCards(images, popup);
 
     popup.addEventListener('click', (e) => {
@@ -34,7 +77,7 @@
     });
   }
 
-  function loadImages(imagesData) {
+  function buildImages(imagesData) {
     return imagesData.map((data) => {
       const { desc, url, fullUrl } = data;
       const img = new Image();
@@ -45,19 +88,47 @@
     });
   }
 
-  function popupImage(el, image) {
+  function popupImage(el, image, attempts=0) {
     const img = new Image();
     img.src = image.getAttribute('data-url-full');
 
     const box = el.children[0];
 
-    box.children[0].innerHTML = '';
-    box.children[1].innerHTML = '';
+    box.innerHTML = '';
 
-    box.children[0].appendChild(img);
-    img.onload = () =>
-      box.children[1].innerText = image.getAttribute('data-desc');
+    const imgBox = document.createElement('div');
+    const imgDesc = document.createElement('div');
+
+    imgBox.classList.add('popup-img-box');
+    imgDesc.classList.add('popup-desc');
+
+    box.appendChild(imgBox);
+    box.appendChild(imgDesc);
+
+    imgBox.appendChild(img);
+
+    img.addEventListener('error', () => {
+      if (attempts >= 3) {
+        box.children[1].innerText = '';
+      } else {
+        popupImage(el, image, attempts + 1);
+      }
+    });
+
+    onsizeload(img, () => {
+      imgDesc.innerText = image.getAttribute('data-desc');
+    });
+
     el.classList.add('show');
+  }
+
+  function onsizeload(image, cb) {
+    const poll = setInterval(() => {
+      if (image.naturalWidth) {
+        clearInterval(poll);
+        cb();
+      }
+    }, 10);
   }
 
   function detectScrollWidth() {
